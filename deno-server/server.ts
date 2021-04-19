8db3f223d8667b1b0ff21b2ed7926dde2d82b2d1
daumkuchen <nodima05@gmail.com>
2021-04-19 15:41:14 +0900

    update deno-server

--- deno-server/server.ts
+++ deno-server/server.ts
@@ -1,11 +1,11 @@
 import { serve } from "https://deno.land/std@0.89.0/http/server.ts";
 
-const port = 8000;
+const port = 8080;
 const http_server = serve({
   port: port
 });
 
-console.log("http://localhost:8000/");
+console.log("http://localhost:8080/");
 
 for await (const req of http_server) {
   req.respond({
